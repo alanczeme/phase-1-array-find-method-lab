6f0d3052d49b89ab3cbbc72e2070bb993454c63b
Alan Czemerinski <alanczeme@gmail.com>
2022-01-12 17:05:10 -0500

    commit tree 86eb48b813c02568224d8f37cbae41681ebf25e2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(records) {
+    const winningObj = records.find(record => record.result === "W");
+    
+    return (winningObj ? winningObj.year : winningObj)
+}
